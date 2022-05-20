7ded5d6bc4f4d54b7e50854a828276a34c6c8ded
Nelson Marques <nelsondante89@gmail.com>
2022-05-19 21:09:39 -0400

    multiple layers

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,38 +1,8 @@
 // Add console.log to check to see if our code is working.
-console.log("working");
+// console.log("working");
 
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
-
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
-
-// Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport, {
-    onEachFeature: function(feature, layer) {
-      console.log(layer);
-      layer.bindPopup();
-    }
-
-  }).addTo(map);
-
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -40,6 +10,24 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     maxZoom: 18,
     accessToken: API_KEY
 });
+ streets.addTo(map);
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/nmarques89/Mapping_Earthquakes/blob/Mapping_GeoJSON_Points/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data,{
+    onEachFeature: function(feature, layer) {
+      console.log(layer);
+      layer.bindPopup("<h3>" + "Airport Code: " + feature.properties.faa +
+      "</h3><hr><p>" + feature.properties.name + "</p>");
+    }    
+  }).addTo(map);
+  });
+
